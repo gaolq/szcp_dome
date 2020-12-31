4dc7c6e6534a7fb7762e7d9a69804d85f4fe6929
Administrator <1095737364@qq.cpm>
2020-12-31 22:07:25 +0800

    首次提交

---
+++ szcp/src/main/java/com/yys/szcp/mapper/DbAdminUserMapper.java
@@ -0,0 +1,75 @@
+package com.yys.szcp.mapper;
+
+import com.yys.szcp.entity.DbAdminUser;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface DbAdminUserMapper {
+
+
+    /**
+     * 添加管理员用户
+     * @param adminUser
+     * @return
+     */
+    int addAdminUser(DbAdminUser adminUser);
+
+    /**
+     * 分页通过机构id查询管理员用户
+     * @param map
+     * @return
+     */
+    List<Map> findAdminUserListByOrganId(Map map);
+    int findAdminUserListByOrganIdCount(Map map);
+
+    /**
+     * 登陆查询
+     * @param map
+     * @return
+     */
+    List<DbAdminUser> findAdminUserByUsernamePassword(Map map);
+
+    /**
+     * 更新管理员用户
+     * @param adminUser
+     * @return
+     */
+    int updateAdminUser(DbAdminUser adminUser);
+    int deleteAdminUser(DbAdminUser adminUser);
+
+    /**
+     * 根据id 查询管理员用户返回用户对象
+     * @param adminUserId
+     * @return
+     */
+    DbAdminUser findAdminUserById(Integer adminUserId);
+
+
+    /**
+     * 通过id查询单个管理员用户详细信息
+     * @param adminUserId
+     * @return
+     */
+    Map findAdminUserAllInfoById(Integer adminUserId);
+
+
+    /**
+     * 通过登陆名称查询单个管理员用户
+     * @param adminUserId
+     * @param username
+     * @return
+     */
+  List<DbAdminUser> findAdminUserByAdminName(Integer adminUserId, String username);
+
+    /**
+     * 初始化密码
+     * @param adminUser
+     * @return
+     */
+  int updatePasswordAdminUser(DbAdminUser adminUser);
+
+
+}
